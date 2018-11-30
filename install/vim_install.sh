fccf3a9c55dcb5e022a344de7bf6f7d15c9eceba
oonota <taotaotao0310@gmail.com>
2018-11-30 17:23:01 +0900

    make vim install file

---
+++ install/vim_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# dein install
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+
+mkdir -pv ${HOME}/.vim
+sh ./installer.sh ${HOME}/.vim/dein
+
+rm -f ./install.sh
+
+
