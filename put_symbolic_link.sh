d462fa47a8128779c976688e642725cc3c5540b0
oonota <taotaotao0310@gmail.com>
2018-12-04 17:01:31 +0900

    コメントを外した

--- put_symbolic_link.sh
+++ put_symbolic_link.sh
@@ -6,7 +6,7 @@ DOTFILES_DIR=$(pwd)
 for file in ${DOTFILES[@]}
 do
 	dot_file=.${file}
-	echo "ln -sfv ${DOTFILES_DIR}/${file} ${HOME}/${dot_file}"
+	ln -sfv ${DOTFILES_DIR}/${file} ${HOME}/${dot_file}
 	
 done 
 
